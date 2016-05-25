b20f26cf7396e40c453c6c7bf8f4ebd2f13dd0f9
AlexDenis <alexander.denisenko@phystech.edu>
2016-05-25 16:02:22 +0300

    Started 2 task by creating data_structure

---
+++ task2/2_sorted_array.c
@@ -0,0 +1,13 @@
+#include "sorted_array.h"
+
+template <class TYPE> class data_structure
+{
+	public:
+	
+	TYPE* data;
+	int memlen;
+	int datalen;
+	
+	 data_structure () { }
+	~data_structure () { }
+};
