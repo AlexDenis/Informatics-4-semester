96f0fdab3777f25efb525af8e69eb6c6f4405e14
AlexDenis <alexander.denisenko@phystech.edu>
2016-05-26 02:52:32 +0300

    added print_exit_message() and pointer_valid()

--- task2/2_sorted_array.h
+++ task2/2_sorted_array.h
@@ -16,3 +16,21 @@ const int MAX_DATASZ = 301; //azaza
 int arrays_count = 0;
 const char* OK_EXITING     = "You've deleted all the arrays.\n";
 const char* NOT_OK_EXITING = "You've not deleted all the arrays.\n'";
+
+void print_exit_message ()
+	{
+	if (arrays_count == 0) printf ("%s", OK_EXITING);
+	else printf ("%s", NOT_OK_EXITING);
+	}
+
+int pointer_valid (void* inp)
+	{
+	if (inp != 0) return 1;
+	else return 0;
+	}
+
+template <class TYPE> class data_structure;
+
+template <class TYPE> class array;
+
+template <class TYPE> class sorted_array;
