648b656f6794d6a64cee780aecada17c84a2a93c
AlexDenis <alexander.denisenko@phystech.edu>
2016-05-25 18:01:06 +0300

    added change_memsz()

--- task2/2_sorted_array.c
+++ task2/2_sorted_array.c
@@ -22,4 +22,26 @@ template <class TYPE> class data_structure
 	}
 	
 	virtual int add_element (TYPE new_element) { }
+	
+	int change_memsz (TYPE newmemlen)
+	{
+		if (memlen == 0)
+		{
+			check (!pointer_valid (data))
+			
+			data = (TYPE*) malloc (sizeof (TYPE) * newmemlen);
+			memlen = newmemlen;
+		}
+	
+		else
+		{
+			check (pointer_valid (data))
+			
+			printf ("reallocating with new mem len %i\n", newmemlen);
+			data = (TYPE*) realloc (data, newmemlen * sizeof (TYPE));
+			memlen = newmemlen;
+		}
+	
+		return 1;
+	}
 };
